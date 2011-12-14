0b578230d408eda0bebdc177ad13b50e7c050f97
bbadjari <bbadjari@e3c1702b-602f-4abb-a9dd-e9ba2762f075>
2011-12-14 03:36:03 +0000

    Marked assembly with attribute to specify neutral language (English) used by resources.

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -27,6 +27,7 @@
 
 using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -64,3 +65,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.1.0")]
 [assembly: AssemblyFileVersion("1.1.1.0")]
+
+// Specify neutral language used by resources in assembly.
+[assembly: NeutralResourcesLanguageAttribute("en")]
